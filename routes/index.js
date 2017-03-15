0641226340b006d45795dc2a3fb2da4b4a7f2b1e
Benjamin Matson <benjaminmatson@gmail.com>
2017-03-15 16:57:34 -0600

    POST route is not pretty, but works

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,22 @@ router.get('/all/:id', (req, res) => {
   let player_id = Number(req.params.id);
   query.get_player(player_id)
     .then(function(player) {
-      query.get_planets()
-        .then(function(planets) {
-          query.get_resources()
-            .then(function(resources) {
-              query.get_planet_resources()
-                .then(function(planet_resources) {
-                  res.status(200).json({
-                    player,
-                    planets,
-                    resources,
-                    planet_resources
-                  })
+      query.get_player_resources(player_id)
+        .then(function(player_resources) {
+          query.get_planets()
+            .then(function(planets) {
+              query.get_resources()
+                .then(function(resources) {
+                  query.get_planet_resources()
+                    .then(function(planet_resources) {
+                      res.status(200).json({
+                        player,
+                        player_resources,
+                        planets,
+                        resources,
+                        planet_resources
+                      })
+                    }).catch(console.error)
                 }).catch(console.error)
             }).catch(console.error)
         }).catch(console.error)
@@ -57,20 +61,44 @@ router.get('/player/:id', (req, res) => {
     }).catch(console.error)
 })
 
-// router.post('/new_player', json_parser, (req, res) => {
-//   console.log(req.body)
-//   let player = req.body.player,
-//     let resources = req.body.resources
-//   query.post_player(player)
-//     .then(function(player) {
-//       query.post_resources(player.id, resources)
-//         .then(function(resources) {
-//           res.status(200).json({
-//             player,
-//             resources
-//           })
-//         }).catch(console.error)
-//     }).catch(console.error)
-// })
+router.post('/player', json_parser, (req, res) => {
+  //console.log(req.body)
+  let player = req.body.player
+  let resources = req.body.resources
+  if (String(req.body.resource) === '[]') {
+    for (let i = 0; i < 10; ++i) {
+      resources.push({
+        'resource_id': i + 1,
+        'qty': 0
+      })
+    }
+  }
+  resources.forEach(function(resource) {
+    if (resource.player_id === undefined) {
+      resource.player_id = player.id
+    }
+  })
+  query.get_player(player.id)
+    .then(function(existing_player) {
+      //console.log('existing player is', existing_player)
+      if (existing_player.length < 1) {
+        query.insert_player(player)
+          .then(function(finished) {
+            query.insert_resources(resources)
+              .then(function(finished) {
+                res.status(200).json(finished)
+              }).catch(console.error)
+          }).catch(console.error)
+      } else {
+        query.update_player(player)
+          .then(function(finished) {
+            query.update_resources(resources)
+              .then(function(finished) {
+                res.status(200).json(finished)
+              }).catch(console.error)
+          }).catch(console.error)
+      }
+    }).catch(console.error)
+})
 
 module.exports = router;
