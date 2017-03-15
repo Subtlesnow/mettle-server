46c29dfa135e364d1a44e4ad5853035db54f69cc
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-03-15 11:06:25 -0600

    Cascaded Foreign Keys

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,21 @@
-
+require('dotenv').config();
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/mettle'
   },
-
+  staging: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    }
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + '?ssl=true'
   }
-
 };
