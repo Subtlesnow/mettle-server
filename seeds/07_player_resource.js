1058c992d1490ac0fe335e9206c8e2bd930be5cc
Benjamin Matson <benjaminmatson@gmail.com>
2017-03-15 13:14:37 -0600

    GET routes working. POST routes disabled

--- seeds/07_player_resource.js
+++ seeds/07_player_resource.js
@@ -7,8 +7,44 @@ exports.seed = function(knex, Promise) {
       //Not sure about this seed, please check me!
       const player_resources = [{
         player_id: 1,
-        resource_id: 2,
+        resource_id: 1,
         qty: 2
+      },{
+        player_id: 1,
+        resource_id: 2,
+        qty: 1
+      },{
+        player_id: 1,
+        resource_id: 3,
+        qty: 0
+      },{
+        player_id: 1,
+        resource_id: 4,
+        qty: 0
+      },{
+        player_id: 1,
+        resource_id: 5,
+        qty: 0
+      },{
+        player_id: 1,
+        resource_id: 6,
+        qty: 0
+      },{
+        player_id: 1,
+        resource_id: 7,
+        qty: 0
+      },{
+        player_id: 1,
+        resource_id: 8,
+        qty: 0
+      },{
+        player_id: 1,
+        resource_id: 9,
+        qty: 0
+      },{
+        player_id: 1,
+        resource_id: 10,
+        qty: 0
       }];
 
       return knex('player_resource').insert(player_resources);
